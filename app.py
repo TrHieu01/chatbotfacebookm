7dd532405e062395a0c230f68eef388bd0313a2a
TrungHieu <hieuanthanh99@gmail.com>
2025-06-10 15:32:43 +0700

    app.py

--- app.py
+++ app.py
@@ -104,5 +104,5 @@ def send_message(recipient_id, message_text):
 
 # Run the app
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False)
